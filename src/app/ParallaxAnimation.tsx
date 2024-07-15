c1128c4a8d869ab962bf67fccc3238703101c8c4
Leonam de Paula <leonamjlpaula@gmail.com>
2024-07-15 10:33:03 -0300

    Parallax animation

--- src/app/ParallaxAnimation.tsx
+++ src/app/ParallaxAnimation.tsx
@@ -1,8 +1,28 @@
-import { Dimensions, ScrollView, StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import Animated, {
+  useAnimatedRef,
+  useScrollViewOffset,
+} from 'react-native-reanimated';
 
 import { ListImage } from '../components/ListImage';
 
 const Images = [
+  'https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1513883524931-aaab83bcb19b?q=100&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1513883524931-aaab83bcb19b?q=100&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1513883524931-aaab83bcb19b?q=100&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1513883524931-aaab83bcb19b?q=100&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1712174863129-dcbd52938915?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1621897100070-055b183ead92?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1623093386041-a0915e5a1ca4?q=100&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -18,30 +38,46 @@ const ItemContainerWidth = ListImageWidth + ItemInternalPadding * 2;
 const ListPadding = (WindowWidth - ItemContainerWidth) / 2;
 
 const ParallaxAnimation = () => {
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollOffset = useScrollViewOffset(scrollRef);
+
+  //How to listen do scrollOffset and log
+  //   useAnimatedReaction(
+  //     () => scrollOffset.value,
+  //     offset => {
+  //       console.log(offset);
+  //     },
+  //   );
+
   return (
     <View style={styles.container}>
-      <ScrollView
+      <Animated.ScrollView
+        ref={scrollRef}
         style={{ flex: 1 }}
         horizontal
         pagingEnabled
         snapToInterval={ItemContainerWidth}
         decelerationRate={'fast'}
+        showsHorizontalScrollIndicator={false}
         contentContainerStyle={{
           alignItems: 'center',
           paddingLeft: ListPadding,
           paddingRight: ListPadding,
         }}>
-        {Images.map(uri => (
+        {Images.map((uri, index) => (
           <ListImage
+            index={index}
             uri={uri}
             key={uri}
-            itemWidth={ListImageWidth}
+            imageWidth={ListImageWidth}
+            itemWidth={ItemContainerWidth}
             style={{
               marginHorizontal: ItemInternalPadding,
             }}
+            scrollOffset={scrollOffset}
           />
         ))}
-      </ScrollView>
+      </Animated.ScrollView>
     </View>
   );
 };
--- src/components/ListImage.tsx
+++ src/components/ListImage.tsx
@@ -1,20 +1,88 @@
 import type { StyleProp, ViewStyle } from 'react-native';
-import { Image, View } from 'react-native';
+import { Dimensions, View } from 'react-native';
+import type { SharedValue } from 'react-native-reanimated';
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 type ListImageProps = {
   uri: string;
+  imageWidth: number;
   itemWidth: number;
   style: StyleProp<ViewStyle>;
+  scrollOffset: SharedValue<number>;
+  index: number;
 };
+const { width: screenWidth } = Dimensions.get('window');
 
-export const ListImage = ({ uri, itemWidth, style }: ListImageProps) => {
+const ParallaxIntensity = screenWidth / 2;
+
+export const ListImage = ({
+  uri,
+  imageWidth,
+  itemWidth,
+  style,
+  scrollOffset,
+  index,
+}: ListImageProps) => {
+  const inputRange = [
+    itemWidth * (index - 1),
+    itemWidth * index,
+    itemWidth * (index + 1),
+  ];
+
+  const rImageStyle = useAnimatedStyle(() => {
+    const translateX = interpolate(scrollOffset.value, inputRange, [
+      -ParallaxIntensity,
+      0,
+      ParallaxIntensity,
+    ]);
+
+    return {
+      transform: [
+        {
+          scale: 1.7,
+        },
+        {
+          translateX,
+        },
+      ],
+    };
+  });
+
+  const rContainerStyle = useAnimatedStyle(() => {
+    const scale = interpolate(scrollOffset.value, inputRange, [1, 1.05, 1]);
+
+    return {
+      transform: [
+        {
+          scale,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={style}>
-      <Image
+    <Animated.View
+      style={[
+        style,
+        { overflow: 'hidden', borderRadius: 20 },
+        rContainerStyle,
+      ]}>
+      <Animated.Image
         key={uri}
         source={{ uri }}
-        style={{ width: itemWidth, aspectRatio: 0.6, borderRadius: 20 }}
+        style={[
+          {
+            width: imageWidth,
+            aspectRatio: 0.6,
+            resizeMode: 'cover',
+            borderRadius: 20,
+          },
+          rImageStyle,
+        ]}
       />
-    </View>
+    </Animated.View>
   );
 };
