1aa325d43dc0c4fc34dcb2e82969c9f5a118894d
Leonam de Paula <leonamjlpaula@gmail.com>
2024-07-18 08:23:39 -0300

    Addea Skia Text

--- src/app/AnimatedText.tsx
+++ src/app/AnimatedText.tsx
@@ -1,7 +1,15 @@
 import { FontAwesome } from '@expo/vector-icons';
 
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import AnimateableText from 'react-native-animateable-text';
+import {
+  Text as SkText,
+  Canvas,
+  useFont,
+  vec,
+  SweepGradient,
+} from '@shopify/react-native-skia';
+import { useState } from 'react';
+import { StyleSheet, Switch, TouchableOpacity, View, Text } from 'react-native';
+// import AnimateableText from 'react-native-animateable-text';
 import {
   useDerivedValue,
   useSharedValue,
@@ -9,7 +17,11 @@ import {
 } from 'react-native-reanimated';
 import { ReText } from 'react-native-redash';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sfProBold = require('../assets/SF-Pro-Rounded-Bold.ttf');
+
 const AnimatedText = () => {
+  const [toggled, setToggled] = useState(false);
   const count = useSharedValue(0);
 
   const randomCount = () => {
@@ -21,10 +33,52 @@ const AnimatedText = () => {
     return Math.floor(count.value).toString();
   });
 
+  const fontSize = 150;
+  const font = useFont(sfProBold, fontSize);
+
+  const canvasWidth = 400;
+  const canvasHeight = 400;
+
+  const x = useDerivedValue(() => {
+    const textWidth = font?.measureText(countString.value).width ?? 0;
+    return canvasWidth / 2 - textWidth / 1.85;
+  }, [font]);
+
+  const y = useDerivedValue(() => {
+    return canvasHeight / 2 + fontSize / 2.8;
+  }, [fontSize]);
+
+  const c = useDerivedValue(() => {
+    return vec(x.value, y.value);
+  });
+
   return (
     <View style={styles.container}>
-      <ReText style={styles.count} text={countString} />
-      {/* <AnimateableText style={styles.count} text={countString} /> */}
+      <Switch
+        onValueChange={() => {
+          setToggled(t => !t);
+        }}
+        value={toggled}
+        style={{ position: 'absolute', top: 50, height: 30 }}
+      />
+      <Text style={styles.text}>{toggled ? 'Skia' : 'ReText'}</Text>
+      {!toggled && <ReText style={styles.count} text={countString} />}
+      {toggled && (
+        <Canvas
+          style={{
+            width: canvasWidth,
+            height: canvasHeight,
+            backgroundColor: 'black',
+          }}>
+          <SkText font={font} color={'black'} text={countString} x={x} y={y}>
+            <SweepGradient
+              c={c}
+              colors={['cyan', 'magenta', 'yellow', 'cyan']}
+            />
+          </SkText>
+        </Canvas>
+      )}
+      {/* <AnimateableText style={styles.count} text={countString} /> //PREBUILD NEEDED!!!*/}
       <TouchableOpacity onPress={randomCount} style={styles.floatingButton}>
         <FontAwesome name="random" size={24} color="white" />
       </TouchableOpacity>
@@ -35,24 +89,34 @@ const AnimatedText = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  floatingButton: {
-    position: 'absolute',
-    bottom: 48,
-    right: 20,
     backgroundColor: 'black',
-    padding: 20,
-    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   count: {
-    fontSize: 80,
+    fontSize: 150,
     fontWeight: 'bold',
     fontFamily: 'SFProRoundedBold',
     width: 200,
     textAlign: 'center',
+    color: 'white',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily: 'SFProRoundedBold',
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 48,
+    right: 32,
+    width: 64,
+    aspectRatio: 1,
+    borderRadius: 32,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
-
 export default AnimatedText;
